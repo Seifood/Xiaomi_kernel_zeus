d97d5b28251d7226281053d236d9f169b8fa908e
Droneship <81514881+Seifood@users.noreply.github.com>
2022-05-07 10:44:40 +0200

    Update netrani.mk

--- techpack/camera-kernel/config/netrani.mk
+++ techpack/camera-kernel/config/netrani.mk
@@ -1,15 +1,15 @@
 # Settings for compiling netrani camera architecture
 
 # Localized KCONFIG settings
-CONFIG_SPECTRA_ISP := y
-CONFIG_SPECTRA_ICP := y
-CONFIG_SPECTRA_TFE := y
-CONFIG_SPECTRA_CRE := y
-CONFIG_SPECTRA_SENSOR := y
+CONFIG_SPECTRA_ISP := n
+CONFIG_SPECTRA_ICP := n
+CONFIG_SPECTRA_TFE := n
+CONFIG_SPECTRA_CRE := n
+CONFIG_SPECTRA_SENSOR := n
 
 # Flags to pass into C preprocessor
-ccflags-y += -DCONFIG_SPECTRA_ISP=1
-ccflags-y += -DCONFIG_SPECTRA_ICP=1
-ccflags-y += -DCONFIG_SPECTRA_TFE=1
-ccflags-y += -DCONFIG_SPECTRA_CRE=1
-ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
+ccflags-y += -DCONFIG_SPECTRA_ISP=0
+ccflags-y += -DCONFIG_SPECTRA_ICP=0
+ccflags-y += -DCONFIG_SPECTRA_TFE=0
+ccflags-y += -DCONFIG_SPECTRA_CRE=0
+ccflags-y += -DCONFIG_SPECTRA_SENSOR=0
