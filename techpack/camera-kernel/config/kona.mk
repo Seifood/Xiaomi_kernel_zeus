c04ffa56e589bdb131e22405caaab7f6f61ac7b1
Droneship <81514881+Seifood@users.noreply.github.com>
2022-05-07 10:43:41 +0200

    Update kona.mk

--- techpack/camera-kernel/config/kona.mk
+++ techpack/camera-kernel/config/kona.mk
@@ -1,15 +1,15 @@
 # Settings for compiling kona camera architecture
 
 # Localized KCONFIG settings
-CONFIG_SPECTRA_FD := y
-CONFIG_SPECTRA_ISP := y
-CONFIG_SPECTRA_ICP := y
-CONFIG_SPECTRA_JPEG := y
-CONFIG_SPECTRA_SENSOR := y
+CONFIG_SPECTRA_FD := n
+CONFIG_SPECTRA_ISP := n
+CONFIG_SPECTRA_ICP := n
+CONFIG_SPECTRA_JPEG := n
+CONFIG_SPECTRA_SENSOR := n
 
 # Flags to pass into C preprocessor
-ccflags-y += -DCONFIG_SPECTRA_FD=1
-ccflags-y += -DCONFIG_SPECTRA_ISP=1
-ccflags-y += -DCONFIG_SPECTRA_ICP=1
-ccflags-y += -DCONFIG_SPECTRA_JPEG=1
-ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
+ccflags-y += -DCONFIG_SPECTRA_FD=0
+ccflags-y += -DCONFIG_SPECTRA_ISP=0
+ccflags-y += -DCONFIG_SPECTRA_ICP=0
+ccflags-y += -DCONFIG_SPECTRA_JPEG=0
+ccflags-y += -DCONFIG_SPECTRA_SENSOR=0
