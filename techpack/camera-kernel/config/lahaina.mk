8b5e9b5a3173b8bb382ae17c6086cd1a37fbe699
Droneship <81514881+Seifood@users.noreply.github.com>
2022-05-07 10:42:16 +0200

    Update lahaina.mk

--- techpack/camera-kernel/config/lahaina.mk
+++ techpack/camera-kernel/config/lahaina.mk
@@ -1,15 +1,15 @@
 # Settings for compiling lahaina camera architecture
 
 # Localized KCONFIG settings
-CONFIG_SPECTRA_ISP := y
-CONFIG_SPECTRA_ICP := y
-CONFIG_SPECTRA_JPEG := y
-CONFIG_SPECTRA_CUSTOM := y
-CONFIG_SPECTRA_SENSOR := y
+CONFIG_SPECTRA_ISP := n
+CONFIG_SPECTRA_ICP := n
+CONFIG_SPECTRA_JPEG := n
+CONFIG_SPECTRA_CUSTOM := n
+CONFIG_SPECTRA_SENSOR := n
 
 # Flags to pass into C preprocessor
-ccflags-y += -DCONFIG_SPECTRA_ISP=1
-ccflags-y += -DCONFIG_SPECTRA_ICP=1
-ccflags-y += -DCONFIG_SPECTRA_JPEG=1
-ccflags-y += -DCONFIG_SPECTRA_CUSTOM=1
-ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
+ccflags-y += -DCONFIG_SPECTRA_ISP=0
+ccflags-y += -DCONFIG_SPECTRA_ICP=0
+ccflags-y += -DCONFIG_SPECTRA_JPEG=0
+ccflags-y += -DCONFIG_SPECTRA_CUSTOM=0
+ccflags-y += -DCONFIG_SPECTRA_SENSOR=0
