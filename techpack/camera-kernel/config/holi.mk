c7cd8950012321904312b9afe9e3c7c6c3f8428a
Droneship <81514881+Seifood@users.noreply.github.com>
2022-05-07 10:43:14 +0200

    Update holi.mk

--- techpack/camera-kernel/config/holi.mk
+++ techpack/camera-kernel/config/holi.mk
@@ -1,13 +1,13 @@
 # Settings for compiling holi camera architecture
 
 # Localized KCONFIG settings
-CONFIG_SPECTRA_ISP := y
-CONFIG_SPECTRA_OPE := y
-CONFIG_SPECTRA_TFE := y
-CONFIG_SPECTRA_SENSOR := y
+CONFIG_SPECTRA_ISP := n
+CONFIG_SPECTRA_OPE := n
+CONFIG_SPECTRA_TFE := n
+CONFIG_SPECTRA_SENSOR := n
 
 # Flags to pass into C preprocessor
-ccflags-y += -DCONFIG_SPECTRA_ISP=1
-ccflags-y += -DCONFIG_SPECTRA_OPE=1
-ccflags-y += -DCONFIG_SPECTRA_TFE=1
-ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
+ccflags-y += -DCONFIG_SPECTRA_ISP=0
+ccflags-y += -DCONFIG_SPECTRA_OPE=0
+ccflags-y += -DCONFIG_SPECTRA_TFE=0
+ccflags-y += -DCONFIG_SPECTRA_SENSOR=0
