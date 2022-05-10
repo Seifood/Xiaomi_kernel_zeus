a1ed8125c5a5febf48b45f366050c5ddb2579eca
Droneship <81514881+Seifood@users.noreply.github.com>
2022-05-10 18:54:44 +0200

    Update core.c

--- drivers/usb/gadget/udc/core.c
+++ drivers/usb/gadget/udc/core.c
@@ -1195,7 +1195,7 @@ static inline void usb_gadget_enable_async_callbacks(struct usb_udc *udc)
 
         struct usb_gadget *gadget = udc->gadget;
 
-        if (gadget->ops->udc_async_callbacks)
+        if (gadget->ops->
                 gadget->ops->udc_async_callbacks(gadget, false);
 }
 
